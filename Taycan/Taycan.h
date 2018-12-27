25a950885672ec55d77835863ee8fd18adfa8d70
L1MeN9Yu <baal998.mophisto@gmail.com>
2018-12-27 19:46:04 +0800

    Initial Commit

---
+++ Taycan/Taycan.h
@@ -0,0 +1,19 @@
+//
+//  Taycan.h
+//  Taycan
+//
+//  Created by Mengyu Li on 2018/12/27.
+//  Copyright © 2018 Limengyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Taycan.
+FOUNDATION_EXPORT double TaycanVersionNumber;
+
+//! Project version string for Taycan.
+FOUNDATION_EXPORT const unsigned char TaycanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Taycan/PublicHeader.h>
+
+
