837c57daea3dbaace0348eb61ea979355e8b6d25
L1MeN9Yu <baal998.mophisto@gmail.com>
2019-06-21 15:07:00 +0800

    git submodule

--- Vendor/xmake.lua
+++ Vendor/xmake.lua
@@ -1,1 +1,7 @@
-includes("lmdb/xmake.lua")
+target("lmdb")
+    set_kind("static")
+    
+    add_files("lmdb/libraries/liblmdb/mdb.c")
+    add_files("lmdb/libraries/liblmdb/midl.c")
+
+    add_headerfiles("lmdb/libraries/liblmdb/lmdb.h")
