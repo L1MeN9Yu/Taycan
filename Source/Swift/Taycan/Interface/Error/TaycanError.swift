b15157e3e353fc7aa4f517b69013f9aece9b8618
L1MeN9Yu <baal998.mophisto@gmail.com>
2019-06-21 14:11:40 +0800

    format

--- Source/Swift/Taycan/Interface/Error/TaycanError.swift
+++ Source/Swift/Taycan/Interface/Error/TaycanError.swift
@@ -41,32 +41,57 @@ public enum TaycanError: Error {
     init(returnCode: Int32) {
 
         switch returnCode {
-        case TAYCAN_KEYEXIST: self = .keyExists
-        case TAYCAN_NOTFOUND: self = .notFound
-        case TAYCAN_PAGE_NOTFOUND: self = .pageNotFound
-        case TAYCAN_CORRUPTED: self = .corrupted
-        case TAYCAN_PANIC: self = .panic
-        case TAYCAN_VERSION_MISMATCH: self = .versionMismatch
-        case TAYCAN_INVALID: self = .invalid
-        case TAYCAN_MAP_FULL: self = .mapFull
-        case TAYCAN_DBS_FULL: self = .dbsFull
-        case TAYCAN_READERS_FULL: self = .readersFull
-        case TAYCAN_TLS_FULL: self = .tlsFull
-        case TAYCAN_TXN_FULL: self = .txnFull
-        case TAYCAN_CURSOR_FULL: self = .cursorFull
-        case TAYCAN_PAGE_FULL:  self = .pageFull
-        case TAYCAN_MAP_RESIZED: self = .mapResized
-        case TAYCAN_INCOMPATIBLE: self = .incompatible
-        case TAYCAN_BAD_RSLOT: self = .badReaderSlot
-        case TAYCAN_BAD_TXN: self = .badTransaction
-        case TAYCAN_BAD_VALSIZE: self = .badValueSize
-        case TAYCAN_BAD_DBI: self = .badDBI
+        case TAYCAN_KEYEXIST:
+            self = .keyExists
+        case TAYCAN_NOTFOUND:
+            self = .notFound
+        case TAYCAN_PAGE_NOTFOUND:
+            self = .pageNotFound
+        case TAYCAN_CORRUPTED:
+            self = .corrupted
+        case TAYCAN_PANIC:
+            self = .panic
+        case TAYCAN_VERSION_MISMATCH:
+            self = .versionMismatch
+        case TAYCAN_INVALID:
+            self = .invalid
+        case TAYCAN_MAP_FULL:
+            self = .mapFull
+        case TAYCAN_DBS_FULL:
+            self = .dbsFull
+        case TAYCAN_READERS_FULL:
+            self = .readersFull
+        case TAYCAN_TLS_FULL:
+            self = .tlsFull
+        case TAYCAN_TXN_FULL:
+            self = .txnFull
+        case TAYCAN_CURSOR_FULL:
+            self = .cursorFull
+        case TAYCAN_PAGE_FULL:
+            self = .pageFull
+        case TAYCAN_MAP_RESIZED:
+            self = .mapResized
+        case TAYCAN_INCOMPATIBLE:
+            self = .incompatible
+        case TAYCAN_BAD_RSLOT:
+            self = .badReaderSlot
+        case TAYCAN_BAD_TXN:
+            self = .badTransaction
+        case TAYCAN_BAD_VALSIZE:
+            self = .badValueSize
+        case TAYCAN_BAD_DBI:
+            self = .badDBI
 
-        case EINVAL: self = .invalidParameter
-        case ENOSPC: self = .outOfDiskSpace
-        case ENOMEM: self = .outOfMemory
-        case EIO: self = .ioError
-        case EACCES: self = .accessViolation
+        case EINVAL:
+            self = .invalidParameter
+        case ENOSPC:
+            self = .outOfDiskSpace
+        case ENOMEM:
+            self = .outOfMemory
+        case EIO:
+            self = .ioError
+        case EACCES:
+            self = .accessViolation
 
         default: self = .other(returnCode: returnCode)
         }
